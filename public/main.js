e722f5fe6449b3bc961239af32a31d2e7c07a244
Sparks <dsparks@proforma.com>
2018-03-12 14:49:16 -0400

    html and css setup

--- public/main.js
+++ public/main.js
@@ -1,5 +1,20 @@
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  // document.querySelector('h1').textContent += '?'
+
+  const team1Name = "Team 1";
+  const team2Name = "Team 2";
+  const score = 0;
+}
+
+
+let count = 0;
+
+const clicked = ()=>{
+
+  count++;
+  console.log("Button was Clicked");
+  const updateScore = document.querySelector('.team-scores>.first-team-score');
+  updateScore.textContent = score +count;
 }
 
 document.addEventListener('DOMContentLoaded', main)
