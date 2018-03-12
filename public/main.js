4301ec0bf099c82fb26a7b3093efc559d1bebfb4
Sparks <dsparks@proforma.com>
2018-03-12 15:39:58 -0400

    add subtract function

--- public/main.js
+++ public/main.js
@@ -3,18 +3,35 @@ const main = () => {
 
   const team1Name = "Team 1";
   const team2Name = "Team 2";
-  const score = 0;
+
 }
 
-
-let count = 0;
-
-const clicked = ()=>{
-
-  count++;
+let team1Total = 0;
+let team2Total = 0;
+const addTeam1 = () => {
+  
+  team1Total++;
   console.log("Button was Clicked");
-  const updateScore = document.querySelector('.team-scores>.first-team-score');
-  updateScore.textContent = score +count;
+  const firstTeamScore = document.querySelector(".first-team-score");
+  firstTeamScore.textContent = team1Total;
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const subTeam1 = () => {
+  team1Total --;
+  console.log("Button was Clicked");
+  const firstTeamScore = document.querySelector(".first-team-score");
+  firstTeamScore.textContent = team1Total;
+}
+const addTeam2 = () => {
+  team2Total++;
+  console.log("Button was Clicked");
+  const secondTeamScore = document.querySelector(".second-team-score");
+  secondTeamScore.textContent = team2Total;
+}
+
+const subTeam2 = () => {
+  team2Total--;
+  console.log("Button was Clicked");
+  const secondTeamScore = document.querySelector(".second-team-score");
+  secondTeamScore.textContent = team2Total;
+}
