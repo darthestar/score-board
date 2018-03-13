a86e2f50cf67a5918b0f5aabc070329f4f6af800
Sparks <dsparks@proforma.com>
2018-03-12 21:51:33 -0400

    finished code for team 2 update

--- public/main.js
+++ public/main.js
@@ -43,3 +43,9 @@ const newTeam1name = document.querySelector(".ut1").value;
 console.log("HELLO")
 team1Name.textContent = newTeam1name;
 }
+
+const updateTeam2 = () => {
+  const newTeam2name = document.querySelector(".ut2").value;
+  console.log("HELLO")
+  team2Name.textContent = newTeam2name;
+}
